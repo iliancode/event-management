9ef4d65f0d9df040adf5f161ce3a98a210a2c8a4
guclusefa <sefaaguclu@gmail.com>
2023-12-11 08:33:50 +0100

    jsaisplus ce que jai fait

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,19 +2,32 @@
 
 namespace App\DataFixtures;
 
+use App\Constants\UserConstants;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
     public function __construct
     (
+        private readonly UserPasswordHasherInterface $userPasswordHasherInterface
     )
     {
     }
 
     public function load(ObjectManager $manager): void
     {
-        // TODO : add fixtures
+        $user = (new User())
+            ->setEmail('admin@esievent.fr')
+            ->setPassword($this->userPasswordHasherInterface->hashPassword(new User(), 'admin'))
+            ->setRoles([UserConstants::ROLE_ADMIN])
+            ->setUsername('admin')
+            ->setFirstname('Admin')
+            ->setLastname('ADIMN')
+            ->setBiography('admin');
+        $manager->persist($user);
+        $manager->flush();
     }
 }
