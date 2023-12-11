8530a8cfc77524c38daa872e53ed48e4fbc3a76b
guclusefa <sefaaguclu@gmail.com>
2023-12-11 09:00:34 +0100

    registration

--- src/Controller/Security/SecurityController.php
+++ src/Controller/Security/SecurityController.php
@@ -30,10 +30,4 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
-
-    #[Route(path: '/logout', name: RouteConstants::ROUTE_REGISTER)]
-    public function logoutTEST(): void
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
 }
