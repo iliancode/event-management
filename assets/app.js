cb37be6a41cdc32ad5e9d1e8881a19d2d9656b5b
guclusefa <sefaaguclu@gmail.com>
2023-12-11 10:48:18 +0100

    stimulus

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,11 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.scss in this case)
-import './styles/app.scss';
-
+import './bootstrap.js';
 import 'bootstrap';
 
+// Styles
+import './styles/app.scss';
+
+// Scripts
 import './scripts/toast.js';
 
-require("bootstrap");
+// Stimulus application
+import './bootstrap';
