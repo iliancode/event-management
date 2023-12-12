09f68a779031425c0cc17df0f435d2adfaed3c70
guclusefa <sefaaguclu@gmail.com>
2023-12-12 16:31:33 +0100

    fixtures

--- migrations/Version20231212153023.php
+++ migrations/Version20231212153023.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231211072529 extends AbstractMigration
+final class Version20231212153023 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,17 @@ final class Version20231211072529 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE event (id INT AUTO_INCREMENT NOT NULL, organizer_id INT NOT NULL, type_id INT NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, date DATETIME NOT NULL, city VARCHAR(255) NOT NULL, zipcode VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, location VARCHAR(255) DEFAULT NULL, drafted TINYINT(1) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL, INDEX IDX_3BAE0AA7876C4DDA (organizer_id), INDEX IDX_3BAE0AA7C54C8C93 (type_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE event (id INT AUTO_INCREMENT NOT NULL, organizer_id INT NOT NULL, type_id INT NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, date DATETIME NOT NULL, city VARCHAR(255) NOT NULL, zipcode VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, location VARCHAR(255) DEFAULT NULL, image_name VARCHAR(255) DEFAULT NULL, image_size INT DEFAULT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL, INDEX IDX_3BAE0AA7876C4DDA (organizer_id), INDEX IDX_3BAE0AA7C54C8C93 (type_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE event_participation (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, event_id INT NOT NULL, fullname VARCHAR(255) DEFAULT NULL, banned TINYINT(1) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL, INDEX IDX_8F0C52E3A76ED395 (user_id), INDEX IDX_8F0C52E371F7E88B (event_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE type (id INT AUTO_INCREMENT NOT NULL, label VARCHAR(255) NOT NULL, max_participants INT DEFAULT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, username VARCHAR(50) NOT NULL, firstname VARCHAR(50) DEFAULT NULL, lastname VARCHAR(50) DEFAULT NULL, biography LONGTEXT DEFAULT NULL, banned TINYINT(1) NOT NULL, verified TINYINT(1) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, username VARCHAR(50) NOT NULL, firstname VARCHAR(50) DEFAULT NULL, lastname VARCHAR(50) DEFAULT NULL, biography LONGTEXT DEFAULT NULL, banned TINYINT(1) NOT NULL, verified TINYINT(1) NOT NULL, image_name VARCHAR(255) DEFAULT NULL, image_size INT DEFAULT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE event ADD CONSTRAINT FK_3BAE0AA7876C4DDA FOREIGN KEY (organizer_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE event ADD CONSTRAINT FK_3BAE0AA7C54C8C93 FOREIGN KEY (type_id) REFERENCES type (id)');
         $this->addSql('ALTER TABLE event_participation ADD CONSTRAINT FK_8F0C52E3A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE event_participation ADD CONSTRAINT FK_8F0C52E371F7E88B FOREIGN KEY (event_id) REFERENCES event (id)');
+        $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
     }
 
     public function down(Schema $schema): void
@@ -38,8 +40,10 @@ final class Version20231211072529 extends AbstractMigration
         $this->addSql('ALTER TABLE event DROP FOREIGN KEY FK_3BAE0AA7C54C8C93');
         $this->addSql('ALTER TABLE event_participation DROP FOREIGN KEY FK_8F0C52E3A76ED395');
         $this->addSql('ALTER TABLE event_participation DROP FOREIGN KEY FK_8F0C52E371F7E88B');
+        $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
         $this->addSql('DROP TABLE event');
         $this->addSql('DROP TABLE event_participation');
+        $this->addSql('DROP TABLE reset_password_request');
         $this->addSql('DROP TABLE type');
         $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE messenger_messages');
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,9 @@
 namespace App\DataFixtures;
 
 use App\Constants\UserConstants;
+use App\Entity\Event;
+use App\Entity\EventParticipation;
+use App\Entity\Type;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -17,17 +20,95 @@ class AppFixtures extends Fixture
     {
     }
 
+    private function makeUser(string $email, string $password, array $roles, string $username, string $firstname, string $lastname, string $biography): User
+    {
+        return (new User())
+            ->setEmail($email)
+            ->setPassword($this->userPasswordHasherInterface->hashPassword(new User(), $password))
+            ->setRoles($roles)
+            ->setUsername($username)
+            ->setFirstname($firstname)
+            ->setLastname($lastname)
+            ->setBiography($biography);
+    }
+
+    private function makeType(string $name, int $max): Type
+    {
+        return (new Type())
+            ->setLabel($name)
+            ->setMaxParticipants($max);
+    }
+
+    private function makeEvent(User $user, Type $type, string $title, string $description, \DateTime $date, string $city, string $zipcode, string $address, string $location): Event
+    {
+        return (new Event())
+            ->setOrganizer($user)
+            ->setType($type)
+            ->setTitle($title)
+            ->setDescription($description)
+            ->setDate($date)
+            ->setCity($city)
+            ->setZipcode($zipcode)
+            ->setAddress($address)
+            ->setLocation($location);
+    }
+
+    private function makeEventParticipation(User $user, Event $event, int $banned) : EventParticipation
+    {
+        return (new EventParticipation())
+            ->setUser($user)
+            ->setEvent($event)
+            ->setBanned($banned);
+    }
+
+    /**
+     * @throws \Exception
+     */
     public function load(ObjectManager $manager): void
     {
-        $user = (new User())
-            ->setEmail('admin@esievent.fr')
-            ->setPassword($this->userPasswordHasherInterface->hashPassword(new User(), 'admin'))
-            ->setRoles([UserConstants::ROLE_ADMIN])
-            ->setUsername('admin')
-            ->setFirstname('Admin')
-            ->setLastname('ADIMN')
-            ->setBiography('admin');
+        // make admin
+        $user = $this->makeUser('admin@esig.fr', 'admin', [UserConstants::ROLE_ADMIN, UserConstants::ROLE_EDITOR, UserConstants::ROLE_USER], 'admin', 'Admin', 'ADIMN', 'admin');
         $manager->persist($user);
         $manager->flush();
+
+        // make 5 editor
+        for ($i = 0; $i < 5; $i++) {
+            $user = $this->makeUser('editor' . $i . '@esig.fr', 'editor' . $i, [UserConstants::ROLE_EDITOR, UserConstants::ROLE_USER], 'editor' . $i, 'Editor' . $i, 'EDITOR' . $i, 'editor' . $i);
+            $manager->persist($user);
+            $manager->flush();
+        }
+
+        // make 20 user
+        for ($i = 0; $i < 20; $i++) {
+            $user = $this->makeUser('user' . $i . '@esig.fr', 'user' . $i, [UserConstants::ROLE_USER], 'user' . $i, 'User' . $i, 'USER' . $i, 'user' . $i);
+            $manager->persist($user);
+            $manager->flush();
+        }
+
+        // make 20 types
+        for ($i = 0; $i < 20; $i++) {
+            $type = $this->makeType('type' . $i, random_int(1, 20));
+            $manager->persist($type);
+            $manager->flush();
+        }
+
+        $users = $manager->getRepository(User::class)->findAll();
+        $types = $manager->getRepository(Type::class)->findAll();
+
+        // make 100 events
+        for ($i = 0; $i < 100; $i++) {
+            $event = $this->makeEvent($users[random_int(0, count($users) - 1)], $types[random_int(0, count($types) - 1)], 'event' . $i, 'event' . $i, new \DateTime(), 'city' . $i, 'zipcode' . $i, 'address' . $i, 'location' . $i);
+            $manager->persist($event);
+            $manager->flush();
+        }
+
+        $events = $manager->getRepository(Event::class)->findAll();
+
+        // make 150 eventParticipation
+        for ($i = 0; $i < 150; $i++) {
+            $eventParticipation = $this->makeEventParticipation($users[random_int(0, count($users) - 1)], $events[random_int(0, count($events) - 1)], random_int(0, 1));
+            $manager->persist($eventParticipation);
+            $manager->flush();
+        }
     }
 }
